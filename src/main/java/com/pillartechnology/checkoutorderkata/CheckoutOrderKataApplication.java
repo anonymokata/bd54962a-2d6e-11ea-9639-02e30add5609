0ca115a52f82ec1178922b153aab547efa69e8a0
someone <someone>
2019-12-08 16:23:38 -0500

    initial commit

---
+++ src/main/java/com/pillartechnology/checkoutorderkata/CheckoutOrderKataApplication.java
@@ -0,0 +1,13 @@
+package com.pillartechnology.checkoutorderkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutOrderKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutOrderKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pillartechnology/checkoutorderkata/CheckoutOrderKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pillartechnology.checkoutorderkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutOrderKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
