ac39c69d23aa5756d429782d66c2d40108081c19
someone <someone>
2019-12-15 14:04:44 -0500

    returns initial CartItem() sale price

--- src/main/java/com/pillartechnology/checkoutorderkata/CartItem.java
+++ src/main/java/com/pillartechnology/checkoutorderkata/CartItem.java
@@ -1,10 +1,18 @@
 package com.pillartechnology.checkoutorderkata;
 
+import java.math.BigDecimal;
+
 public class CartItem extends Item {
+
+	private BigDecimal salePrice = getPrice();
 
 	// Constructor
 	public CartItem(Item item) {
 		super(item.getName(), item.getPrice().toString()); 
+	}
+
+	public BigDecimal getSalePrice() {
+		return salePrice;
 	}
 
 } // End CartItem()
--- src/test/java/com/pillartechnology/checkoutorderkata/TestCartItem.java
+++ src/test/java/com/pillartechnology/checkoutorderkata/TestCartItem.java
@@ -18,5 +18,15 @@ public class TestCartItem {
 		
 	}
 	
+	@Test
+	public void shouldReturnInitialCartItemSalePrice() {
+		Item item1 = new Item("Soup", "1.99");
+		
+		CartItem cartItem1 = new CartItem(item1);
+		
+		assertEquals("1.99", cartItem1.getSalePrice().toString());
+	}
+	
+	
 	
 } // End TestCartItem()
