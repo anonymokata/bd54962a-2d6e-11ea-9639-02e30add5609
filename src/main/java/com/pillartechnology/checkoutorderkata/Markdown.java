5fa6d45908fb19e05c78315fb33e6d8ef6653766
someone <someone>
2019-12-15 14:29:43 -0500

    can get markdown amount; update constructor

--- src/main/java/com/pillartechnology/checkoutorderkata/Markdown.java
+++ src/main/java/com/pillartechnology/checkoutorderkata/Markdown.java
@@ -4,12 +4,17 @@ import java.math.BigDecimal;
 
 public class Markdown {
 
+	private String description;
 	private BigDecimal markdownAmount;
 
-	public Markdown(String markdownAmount) {
+	public Markdown(String description, String markdownAmount) {
+		this.description = description;
 		this.markdownAmount = new BigDecimal(markdownAmount);
 	}
 
-	
+	public BigDecimal getMarkdownAmount() {
+		return markdownAmount;
+	}
+
 	
 } // End Markdown()
---
+++ src/test/java/com/pillartechnology/checkoutorderkata/TestMarkdown.java
@@ -0,0 +1,19 @@
+package com.pillartechnology.checkoutorderkata;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestMarkdown {
+
+	@Test
+	public void shouldReturnMarkdownAmountAsBigDecimal() {
+		Markdown markdown = new Markdown("$1.00 OFF", "1.00");
+		
+		assertEquals("1.00", markdown.getMarkdownAmount().toString());
+	}
+	
+	
+} // End TestMarkdown()
