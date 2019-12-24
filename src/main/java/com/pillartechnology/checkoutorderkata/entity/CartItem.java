9febafc0b90d59b78a80abbe5555764a183f8632
someone <someone>
2019-12-24 09:07:38 -0500

    code documentation

--- src/main/java/com/pillartechnology/checkoutorderkata/entity/CartItem.java
+++ src/main/java/com/pillartechnology/checkoutorderkata/entity/CartItem.java
@@ -6,25 +6,48 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * CartItem stores an Item that is added to a Cart.
+ * CartItem stores an {@link Item} that is added to a {@link Cart}.
+ * <p>This stores the final sale price and if the {@link Item}
+ * isChargeByWeight, stores that weight in this.</p>
  *
+ *@version 0.1.0
  */
 public class CartItem {
 
 	Logger logger = LoggerFactory.getLogger(CartItem.class);
 	
 	private Item item;
+	/**
+	 * Stores the calculated selling price
+	 * of the CartItem. An applied {@link Markdown}
+	 * will reduce the {@link Item} selling price.
+	 */
 	private BigDecimal sellPrice;
+	/**
+	 * If {@link Item} isChargeByWeight, the weight
+	 * of the {@link Item} is stored here.
+	 */
 	private double weight = 0.0;
 
 	/* Constructors */
-	
+	/**
+	 * Class Constructor.
+	 * 
+	 * @param item the {@link Item} reference for this class.
+	 */
 	public CartItem(Item item) {
 		this.item = item;
 		calculateSellPrice();
 		logger.info("Created cart-item with: " + item.toString());
 	}
 
+	/**
+	 * Class Constructor for {@link Item} that isChargeByWeight.
+	 * 
+	 * @param item the {@link Item} reference for this class.
+	 * @param weight the measured weight of the {@link Item}
+	 * that isChargeByWeight.
+	 */
 	public CartItem(Item item, double weight) {
 		this.item = item;
 		this.weight = weight;
@@ -64,7 +87,7 @@ public class CartItem {
 	 * price to the CartItem sell price. If the CartItem
 	 * is charge by weight, the sell price is
 	 * recalculated in the method by multiplying the cartItem.weight
-	 *  x cartItem.sellPrice.
+	 *  * cartItem.sellPrice.
 	 */
 	public void calculateSellPrice() {
 		this.sellPrice = item.getSalePrice();
