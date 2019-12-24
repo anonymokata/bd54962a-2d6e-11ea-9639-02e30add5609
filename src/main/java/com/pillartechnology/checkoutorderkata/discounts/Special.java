0dda7fb5663432524efc1f9fd7819fa763fe5a5b
someone <someone>
2019-12-24 09:55:31 -0500

    code documentation

--- src/main/java/com/pillartechnology/checkoutorderkata/discounts/Special.java
+++ src/main/java/com/pillartechnology/checkoutorderkata/discounts/Special.java
@@ -8,18 +8,52 @@ import com.pillartechnology.checkoutorderkata.entity.Item;
  * Special is the abstract base class for all specials
  * which allows assigning different specials to items.
  *
- *@version 1.0.0
- *@since 1.0.0
+ *@version 0.1.0
  */
 public abstract class Special {
 	
+	/**
+	 * Number of items required to purchase
+	 * in order for the Special to apply.
+	 */
 	private int buyQtyRequirement;
+	
+	/**
+	 * Once the buy quantity is met, this 
+	 * represents the number of items to
+	 * receive at a discounted price.
+	 */
 	protected int receiveQtyItems;
+	
+	/**
+	 * Percentage of the discount entered as a double,
+	 * but as a whole number. The number is converted
+	 * to a decimal later.
+	 * <p><strong>Example:</strong> Enter 50% as 50 or 50.0.</p>
+	 */
 	protected double discountPercentage;
+	
+	/**
+	 * Stores the limit on number of items that a special
+	 * can be applied to the Special. This number includes
+	 * the buyQtyRequirement and receiveQtyItems.
+	 */
 	private int limit;
+	
+	/** 
+	 * Descriptive name used as an identifier.
+	 * <p><strong>Example:</strong> Buy 2 get 1 for half-off</p>
+	 */
 	private String name;
 	
 	// Constructor
+	
+	/**
+	 * Class Constructor
+	 * 
+	 * @param buyQtyRequirement minimum quantity of items needed in cart
+	 * before Special applies.
+	 */
 	public Special(int buyQtyRequirement) {
 		this.buyQtyRequirement = buyQtyRequirement;
 	}
